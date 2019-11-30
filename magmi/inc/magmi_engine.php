45aaaa94867865cc457083e70d6c3203bf0f1933
bertod <bertodattoma@gmail.com>
2019-11-30 12:15:27 +0100

    get env.php data by include

--- magmi/inc/magmi_engine.php
+++ magmi/inc/magmi_engine.php
@@ -508,28 +508,21 @@ abstract class Magmi_Engine extends DbHelper
                 $user = $default_setup->username;
                 $pass = $default_setup->password;
                 $port = $default_setup->port;
-            }else if($conn == 'envphp'){ //berto - scraping env.php
+				
+            }else if($conn == 'envphp'){ //bertod - get env.php
 				$baseDir = $this->getProp('MAGENTO', 'basedir');
                 $envPath = $baseDir.'/app/etc/env.php';
                 if (!file_exists($envPath)) {
                     throw new Exception("Cannot load xml from path '$envPath'");
                 }
-				$content = rtrim(file_get_contents($envPath));
-				$db_info = array();
-				preg_match("/'host'\s*=>\s*'\w*'/",$content,$db_info['host'] );
-				preg_match("/'username'\s*=>\s*'\w*'/",$content,$db_info['username'] );
-				preg_match("/'dbname'\s*=>\s*'\w*'/",$content,$db_info['dbname'] );
-				preg_match("/'password'\s*=>\s*'\w*'/",$content,$db_info['password'] );
-				foreach($db_info as $infoname => $infodata){
-					$tmp_array = explode("=>", $infodata[0]);
-					$db_info[$infoname] = trim($tmp_array[1]);
-				}
-                $host = $db_info['host'];
-                $dbname = $db_info['dbname'];
-                $user = $db_info['username'];
-                $pass = $db_info['password'];
+				$env_array = include $envPath;
+				$host = $env_array['db']['connection']['default']['host'];
+                $dbname = $env_array['db']['connection']['default']['dbname'];
+                $user = $env_array['connection']['default']['username'];
+                $pass = $env_array['connection']['default']['password'];
                 $port = '3306';
-			}else {
+				
+			} else {
                 $host = $this->getProp("DATABASE", "host", "localhost");
                 $dbname = $this->getProp("DATABASE", "dbname", "magento");
                 $user = $this->getProp("DATABASE", "user");
